b14f2fc53a1c19775ece0ed2fc22f58ac9211455
bobbyhadz <opmftw@gmail.com>
2024-03-05 08:40:31 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('bobbyhadz.com');
+
+const input = document.getElementById('first_name');
+
+// ✅ Remove focus from specific element
+input.blur();
+
+// ✅ Remove focus from the currently active element
+// document.activeElement.blur();
